db3a2ef7ec513a8e57499f3e8131425a53ac5483
Nosa Cikal Daputra <nosacikal@gmail.com>
2019-04-13 21:21:35 +0700

    Membuat layout halaman register dan menambahkan assets

--- app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Register.java
+++ app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Register.java
@@ -3,6 +3,13 @@ package com.nosacikal.lat2_akb2_10116062_nosacikaldaputra;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+ *  Tanggal Pengerjaan :  13 April 2019
+ *  Nim : 10116062
+ *  Nama: Nosa Cikal Daputra
+ *  Kelas : AKB2 (IF-2)
+ * */
+
 public class Register extends AppCompatActivity {
 
     @Override
