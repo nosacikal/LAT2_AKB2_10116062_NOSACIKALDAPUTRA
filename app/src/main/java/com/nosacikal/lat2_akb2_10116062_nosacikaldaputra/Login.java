ade73dd107a1fdab7ec9f6537cc1304da0c163d9
Nosa Cikal Daputra <nosacikal@gmail.com>
2019-04-13 20:36:03 +0700

    Navigasi ke halaman register

--- app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Login.java
+++ app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Login.java
@@ -1,7 +1,10 @@
 package com.nosacikal.lat2_akb2_10116062_nosacikaldaputra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 /*
 *  Tanggal Pengerjaan :  13 April 2019
@@ -12,9 +15,25 @@ import android.os.Bundle;
 
 public class Login extends AppCompatActivity {
 
+    private TextView register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigatorRegister();
+            }
+        });
     }
+
+    public void navigatorRegister() {
+        Intent intent = new Intent(Login.this, Register.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Register.java
@@ -0,0 +1,13 @@
+package com.nosacikal.lat2_akb2_10116062_nosacikaldaputra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
