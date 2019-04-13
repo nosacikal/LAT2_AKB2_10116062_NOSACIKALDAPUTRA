53bbb165ac2a352e453e8e99e6ba45e050e15710
Nosa Cikal Daputra <nosacikal@gmail.com>
2019-04-14 06:50:28 +0700

    Membuat layout halaman almost there

--- app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/AlmostThere.java
+++ app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/AlmostThere.java
@@ -1,13 +1,38 @@
 package com.nosacikal.lat2_akb2_10116062_nosacikaldaputra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/*
+ *  Tanggal Pengerjaan :  14 April 2019
+ *  Nim : 10116062
+ *  Nama: Nosa Cikal Daputra
+ *  Kelas : AKB2 (IF-2)
+ * */
 
 public class AlmostThere extends AppCompatActivity {
+
+    private Button btnVerify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_almost_there);
+
+        btnVerify = findViewById(R.id.btnVerify);
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigatorVerifyAccount();
+            }
+        });
+    }
+
+    public void navigatorVerifyAccount() {
+        Intent intent = new Intent(AlmostThere.this, VerifyAccount.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Register.java
+++ app/src/main/java/com/nosacikal/lat2_akb2_10116062_nosacikaldaputra/Register.java
@@ -1,7 +1,10 @@
 package com.nosacikal.lat2_akb2_10116062_nosacikaldaputra;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /*
  *  Tanggal Pengerjaan :  13 April 2019
@@ -12,10 +15,25 @@ import android.os.Bundle;
 
 public class Register extends AppCompatActivity {
 
+    private Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigatorAlmostThere();
+            }
+        });
+    }
+
+    public void navigatorAlmostThere() {
+        Intent intent = new Intent(Register.this, AlmostThere.class);
+        startActivity(intent);
     }
 
 
